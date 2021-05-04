dfd6d7b158250f27168c2ee0f9867793e1c7d91a
tamaturgo <ddst.snf20@uea.edu.br>
2021-05-04 16:27:08 -0400

    Update Tree_Y

--- Atividade002_AquecimentoPython/Tree_Y.py
+++ Atividade002_AquecimentoPython/Tree_Y.py
@@ -1,38 +1,44 @@
-from turtle import *
+import turtle
 
 
-# Definindo velocidade da caneta e virando ela para cima 
-speed('fastest')
-rt(-90)
-# Angulo de inclinação entre as ramificações
-angulo = 30
-
+# Função Principal
 def treeY(tamanho, level):
 
     if level > 0:
-        colormode(255)
+        turtle.colormode(255)
         # Selecionando a cor verde para o ultimo level
-        pencolor(0, 255//level, 0)
+        caneta.pencolor(0, 255//level, 0)
 
         #desenhando a base
-        fd(tamanho)
-        rt(angulo)
+        caneta.forward(tamanho)
+        caneta.right(angulo)
 
         # Criando ramificações da arvore
         treeY(0.8 * tamanho, level-1)
-        pencolor(0, 255//level, 0)
+        caneta.pencolor(0, 255//level, 0)
 
-        lt(2 * angulo)
+        caneta.left(2 * angulo)
         treeY(0.8 * tamanho, level-1)
 
-        pencolor(0, 255//level, 0)
-        rt(angulo)
-        fd(-tamanho)
+        caneta.pencolor(0, 255//level, 0)
+        caneta.right(angulo)
+        caneta.forward(-tamanho)
 
+
+# Angulo de inclinação entre as ramificações
+angulo = 30
 
 # Interação com o Usuário
 lvl = int(input("Digite quantas ramificações sua árvore terá (recomendamos"+
                 " no máximo 15 ramificações): "))
 
+# Criando e definindo velocidade da caneta e virando ela para cima 
+caneta = turtle.Turtle()
+caneta.speed('fastest')
+caneta.right(-90)
+
 # Inicializando a função principal
 treeY(50, lvl)
+
+# Pausar programa ao fim da sua execução
+turtle.done()
